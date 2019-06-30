6d70d5a0303b4d86de91a8c1ba8ac2695e95f32c
sbelyov <51666176+sbelyov@users.noreply.github.com>
2019-06-30 23:03:21 +0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,3 +3,6 @@ sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
 sudo /etc/init.d/mysql start
+
+#sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+#sudo /etc/init.d/nginx restart
